bf87c6d258a9faf1217fc57a82b5aa52cacf6776
MrEkinox <yannis.caussade@gmail.com>
2022-11-23 11:48:44 +0100

    fix: pointer

--- src/where/index.ts
+++ src/where/index.ts
@@ -161,9 +161,9 @@ export const whereCollection = (
             if (!whereID) return acc2;
 
             if (field.type === "Pointer" && whereOperator) {
-              console.log({ fieldName, whereOperator, whereID });
               const targetCollection = getTargetCollection(field.target, []);
               const targetDoc = targetCollection.doc(whereID);
+              console.log({ fieldName, whereOperator, whereID, targetDoc }, targetDoc.path);
 
               return acc2.where(fieldName, whereOperator, targetDoc);
             }
