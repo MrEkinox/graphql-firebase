237f5200adb619143083cfcf4f52fe1f07eaa64c
MrEkinox <yannis.caussade@gmail.com>
2022-12-11 15:40:25 +0100

    improve reference

--- src/reference/index.ts
+++ src/reference/index.ts
@@ -1,35 +1,49 @@
 import { firestore } from "firebase-admin";
-import { GraphQLResolveInfo } from "graphql";
+import { DocumentReference } from "firebase-admin/firestore";
+import { GraphQLResolveInfo, GraphQLSchema } from "graphql";
 import { getCollection, getParents } from "../mutations";
 import { isOnlyIdField } from "../utils";
 
+const getRefCollection = (
+  type: string,
+  ref: DocumentReference | Record<string, any>,
+  schema: GraphQLSchema
+): DocumentReference => {
+  if (ref instanceof DocumentReference) return ref;
+
+  const parents = getParents(type, [], schema);
+  const collection = getCollection(parents);
+
+  return collection.doc(ref.id);
+};
+
 export const referenceResolver = async (
-  target: string,
+  type: string,
   isList: boolean,
   src,
   info: GraphQLResolveInfo
 ) => {
-  const isOnlyId = isOnlyIdField(info)
-
-  const parents = getParents(target, [], info.schema);
-  const collection = getCollection(parents);
+  const isOnlyId = isOnlyIdField(info);
 
   if (isList) {
-    const refs: firestore.DocumentReference[] = src[info.fieldName];
+    const refs: DocumentReference[] = src[info.fieldName];
     if (refs?.length) {
       if (isOnlyId) {
         return refs.map((ref) => ({ id: ref.id }));
       }
-      const collectionRefs = refs.map((ref) => collection.doc(ref.id));
+      const collectionRefs = refs.map((ref) =>
+        getRefCollection(type, ref, info.schema)
+      );
       const snapshot = await firestore().getAll(...collectionRefs);
       return snapshot.map((doc) => doc.data());
     }
     return [];
   }
 
-  const ref: firestore.DocumentReference = src[info.fieldName];
+  const ref: DocumentReference = src[info.fieldName];
   if (!ref) return null;
   if (isOnlyId) return { id: ref.id };
-  const snapshot = await collection.doc(ref.id).get();
+
+  const snapshot = await getRefCollection(type, ref, info.schema).get();
   return snapshot.data();
 };
